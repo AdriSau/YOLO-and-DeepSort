8a419b2ba94a664806ff8fbb412df4c9f4b4d6f5
AdriSau <adrian.saugar@alumnos.upm.es>
2024-02-15 18:29:11 +0100

    cm

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ultralytics import YOLO
 
 from tracker import Tracker
 
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 video_path = "testingFootage/Clip_TopView_720p.mp4"
 video_out_path = "testingFootage/out.mp4"
