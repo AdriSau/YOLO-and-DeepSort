6bbbce4576016ed86410b6277b30173250552413
AdriSau <adrian.saugar@alumnos.upm.es>
2024-02-16 11:40:53 +0100

    Cm

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 
 from tracker import Tracker
 
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 video_path = "testingFootage/Clip_TopView_720p.mp4"
 video_out_path = "testingFootage/out.mp4"
@@ -14,8 +14,7 @@ video_out_path = "testingFootage/out.mp4"
 cap = cv2.VideoCapture(video_path)
 ret, frame = cap.read()
 
-cap_out = cv2.VideoWriter(video_out_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), cap.get(cv2.CAP_PROP_FPS),
-                          (frame.shape[1], frame.shape[0]))
+#cap_out = cv2.VideoWriter(video_out_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), cap.get(cv2.CAP_PROP_FPS),(frame.shape[1], frame.shape[0]))
 
 model = YOLO("best.pt")
 
@@ -25,9 +24,7 @@ colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255
 
 detection_threshold = 0.5
 while ret:
-
     results = model(frame)
-
     for result in results:
         detections = []
         for r in result.boxes.data.tolist():
@@ -46,12 +43,17 @@ while ret:
             bbox = track.bbox
             x1, y1, x2, y2 = bbox
             track_id = track.track_id
-
+            object_center_X = (int(x1)+int(x2))/2
+            object_center_Y = (int(y1)+int(y2))/2
+            print('car detected: id: '+str(track_id)+' detected in cordinates: ('+str(object_center_X)+','+str(object_center_Y)+')')
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (colors[track_id % len(colors)]), 3)
+            cv2.circle(frame,( int(object_center_X),int(object_center_Y)),2,(colors[track_id % len(colors)]),thickness = -1)
 
-    cap_out.write(frame)
+    cv2.imshow('display_video',frame)
+    cv2.waitKey(30)
+    #cap_out.write(frame)
     ret, frame = cap.read()
 
 cap.release()
-cap_out.release()
+#cap_out.release()
 cv2.destroyAllWindows()
